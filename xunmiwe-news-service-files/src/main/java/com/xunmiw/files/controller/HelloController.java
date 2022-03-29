808f569315ed4c406dcad368784b874a8d0f1731
xunmiw17 <xunmiw@cs.washington.edu>
2022-03-29 12:27:45 -0700

    Creates file module for file uploading and downloading

---
+++ xunmiwe-news-service-files/src/main/java/com/xunmiw/files/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.xunmiw.files.controller;
+
+import com.xunmiw.api.controller.user.HelloControllerApi;
+import com.xunmiw.grace.result.GraceJSONResult;
+import com.xunmiw.utils.RedisOperator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController implements HelloControllerApi {
+
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    public Object hello() {
+        return GraceJSONResult.ok("Hello");
+    }
+}
