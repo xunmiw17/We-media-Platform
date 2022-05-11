89e7c5547e540c62456e50e90eac0d4aa661648d
xunmiw17 <xunmiw@cs.washington.edu>
2022-05-11 11:35:59 -0700

    Configure Zuul routing and register the Zuul service to Eureka to enable service discovery when routing

--- springcloud-zuul-server/src/main/java/com/xunmiw/zuul/Application.java
+++ springcloud-zuul-server/src/main/java/com/xunmiw/zuul/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.xunmiw", "org.n3r.idworker"})
 @EnableZuulProxy    // 为@EnableZuulServer的增强版，当Zuul和Eureka、Ribbon等组件共同使用时，则使用@EnableZuulProxy注解
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
